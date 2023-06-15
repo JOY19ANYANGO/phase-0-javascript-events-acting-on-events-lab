1df4de0cf04b88eaaaebb5943aba2521fa1351c6
JOY19ANYANGO <joyanyango402@gmail.com>
2023-06-15 07:54:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  const leftPosition = parseInt(dodger.style.left);
+
+  if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const leftPosition = parseInt(dodger.style.left);
+
+  if (leftPosition < 360) {
+    dodger.style.left = `${leftPosition + 1}px`;
+  }
+}
